f66c18d45c7df178e9cde91c0a07b7a9d7be0876
odarisg <odaris.carreno@gmail.com>
2023-01-28 20:32:16 -0500

    Encriptación

---
+++ encriptar.js
@@ -0,0 +1,43 @@
+function encriptar(){
+  let original = document.getElementById("texto").value;
+  let encriptada="";
+
+  for(i=0; i<original.length; i++){
+    if(original[i]=="a"){
+      encriptada+="ai";
+    }
+    else if(original[i]=="e"){
+      encriptada+="enter";
+    }
+    else if(original[i]=="i"){
+      encriptada+="imes";
+    }
+    else if(original[i]=="o"){
+      encriptada+="ober";
+    }
+    else if(original[i]=="u"){
+      encriptada+="ufat";
+    }
+    else{
+      encriptada+=original[i];
+    }
+  }
+
+  document.getElementById("secundario").innerHTML = encriptada;
+  document.getElementById("texto").value="";
+
+}
+
+function desencriptar(){
+  let encriptada = document.getElementById("texto").value;
+  let original="";
+  encriptada= encriptada.replace(/ai/g,"a");
+  encriptada= encriptada.replace(/enter/g,"e");
+  encriptada= encriptada.replace(/imes/g,"i");
+  encriptada= encriptada.replace(/ober/g,"o");
+  encriptada= encriptada.replace(/ufat/g,"u");
+
+  original=encriptada;
+  document.getElementById("secundario").innerHTML = encriptada;
+  document.getElementById("texto").value="";
+}
